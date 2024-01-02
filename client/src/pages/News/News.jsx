fa18c44190ca04867a650e5b79ce4a28eaae90e3
lucaaramos <lucaramos99@gmail.com>
2024-01-02 16:56:41 -0300

    image

--- client/src/pages/News/News.jsx
+++ client/src/pages/News/News.jsx
@@ -39,7 +39,8 @@ export default function NewsList() {
               <h3 className="newsTitle">{noticia.title}</h3>
               <p className="newsContent">{noticia.content}</p>
               <p className="newsAuthor">Por: {noticia.user.name}</p>
-              <img className='img' src={`${process.env.REACT_APP_URI_API}${noticia.image}`} />
+              <img className='img' src={`${process.env.REACT_APP_URI_API}optimize/${noticia.image}`} />
+              
               <div className='divButtons'>
               {
                   user?.type === "admin" ? <button className='button' onClick={() => handleDelete(noticia.id)}>X</button> :<></>
