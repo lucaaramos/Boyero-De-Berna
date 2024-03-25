5b59d3ec4ebb927d981820aec65e2c6ff788611c
lucaaramos <lucaramos99@gmail.com>
2024-03-25 11:58:12 -0300

    updates

--- app2/config/config.js
+++ app2/config/config.js
@@ -1,8 +1,15 @@
 const mysql = require("mysql2");
 require('dotenv').config();
 
-var conn = mysql.createConnection(process.env.DATABASE_URL)
-conn.connect((err)=>{
+var conn = mysql.createPool({
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    port: process.env.MYSQL_ADDON_PORT,
+    uri: process.env.MYSQL_ADDON_URI,
+})
+conn.getConnection((err)=>{
     if(!err){
         console.log("established connection")
     } else {
